656507a16ea5cbef8960d4f1e08bf96f600471ea
Deoval Junior <deovaljr@gmail.com>
2017-11-16 19:01:27 -0200

    alteracoes no python

--- lercsv.py
+++ lercsv.py
@@ -1,5 +1,12 @@
 import csv, sys, numpy, snap
 
+
+class myarray(numpy.ndarray):
+    def __new__(cls, *args, **kwargs):
+        return numpy.array(*args, **kwargs).view(myarray)
+    def where(self, value):
+        return numpy.where(self==value)[0][0]
+
 def addTeam(graph, teams, name):
 	i = 0
 	for team in teams:
@@ -12,29 +19,29 @@ def addTeam(graph, teams, name):
 	G1.AddNode(i)
 
 def addTeamGames(graph, teams, home, away):
-	print home
 	G1.AddEdge(teams.where(home), teams.where(away))
 
 nome_ficheiro = 'premierleague1617.csv'
-teams = numpy.empty(100, dtype=object)
+teams = myarray([None for x in range(100)], dtype=object)
+
 G1 = snap.TNGraph.New()
 i = 0
 with open(nome_ficheiro, 'rb') as ficheiro:
     reader = csv.reader(ficheiro)
+    cabecalho = next(reader)
     try:
         for linha in reader:
         	addTeam(G1, teams, linha[2])
-        for linha in reader:
-        	addTeamGames(G1, teams, linha[2], linha[3])
     except csv.Error as e:
         sys.exit('ficheiro %s, linha %d: %s' % (nome_ficheiro, reader.line_num, e))
 
 with open(nome_ficheiro, 'rb') as ficheiro:
     reader = csv.reader(ficheiro)
+    cabecalho = next(reader)
     try:
         for linha in reader:
         	addTeamGames(G1, teams, linha[2], linha[3])
     except csv.Error as e:
         sys.exit('ficheiro %s, linha %d: %s' % (nome_ficheiro, reader.line_num, e))
 
-print G1
+print G1.GetEdges();
